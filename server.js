38b565f4bb77fbdcc6b5af4efdcd537fa276cbae
jaxongirmirzo <jaxongirmirzo@gmail.com>
2025-09-21 13:16:58 +0430

    Add CORS middleware

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
+// const express = require("express");
+// const app = express();
+// const PORT = process.env.PORT || 3000;
+
+// const recipes = require("./data.json");
+
+// app.get("/recipes", (req, res) => {
+//   res.json(recipes);
+// });
+
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
 const express = require("express");
+const cors = require("cors");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
+
+app.use(cors()); // <-- Shu yerga qo‘shildi
 
 const recipes = require("./data.json");
 
