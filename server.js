6a99e1fba6b6a11d4204ad16a3fcdb4dfb8f2a7d
jaxongirmirzo <jaxongirmirzo@gmail.com>
2025-09-21 12:39:19 +0430

    Initial commit

--- server.js
+++ server.js
@@ -1,29 +1,13 @@
-import express from "express";
-import fs from "fs";
-
+const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
-// JSON fayldan ma'lumotni o'qish
-const data = JSON.parse(fs.readFileSync("./data.json", "utf-8"));
+const recipes = require("./data.json");
 
-// Barcha retseptlarni qaytarish
 app.get("/recipes", (req, res) => {
-  res.json(data.recipes);
+  res.json(recipes);
 });
 
-// Bitta retsept (id bo‘yicha)
-app.get("/recipes/:id", (req, res) => {
-  const recipeId = parseInt(req.params.id);
-  const recipe = data.recipes[recipeId];
-  if (recipe) {
-    res.json(recipe);
-  } else {
-    res.status(404).json({ message: "Recipe not found" });
-  }
-});
-
-// Serverni ishga tushirish
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
